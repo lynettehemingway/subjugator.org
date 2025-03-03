19788f0d5328f871d1c2c6598eda12278243ea87
pablopupo <145598901+pablopupo@users.noreply.github.com>
2025-03-02 22:50:31 -0500

    Initial commit of Subjugator website

--- src/Pages/OurTeam.jsx
+++ src/Pages/OurTeam.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function OurTeam() {
   return (
     <div>
-      <h1>OurTeam</h1>
+      <h1>Our Team</h1>
       <p>This is a placeholder for now.</p>
     </div>
   );
